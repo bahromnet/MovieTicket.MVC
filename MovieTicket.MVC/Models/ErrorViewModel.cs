6782d402220800018aaf4640b0b6b112b88133c8
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-06-13 07:28:37 +0300

    Add project files.

---
+++ MovieTicket.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MovieTicket.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
